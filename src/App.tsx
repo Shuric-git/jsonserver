44f80b01e835d8970e915dd1b662a3f73f03522e
Shuric-git <ulme@rambler.ru>
2022-10-14 00:21:42 +0300

    sort, search, pagination (have pagination issue)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {db} from "./db";
 
 function App() {
+
+  interface IUser {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: any,
+    phone: string,
+    website: string,
+    company: any,
+  }
+
+  const [data, setData] = useState([])
+  const [order, setOrder] = useState('asc')
+  const [page, setPage] = useState(1)
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+  let users = db.getUsers(page)
+  users.then(data => setData(data))
+  },[])
+
+
+  async function sortBy(param: string, order: string, page: number) {
+    let sortedArr = db.sortBy(param, order, page)
+    sortedArr.then(data => setData(data))
+
+    if (order === 'asc') setOrder('desc')
+    else setOrder('asc')
+  }
+
+  async function search(query: string) {
+    let searchArr = db.search(query)
+    searchArr.then(data => setData(data))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <input type="text"
+           value={input}
+           onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => search(input)}>Search</button>
+      </div>
+      <table className='table'>
+        <tbody>
+          <tr>
+            <th onClick={() => sortBy('name', order, page)}>Name</th>
+            <th onClick={() => sortBy('username', order, page)}>Username</th>
+            <th onClick={() => sortBy('address', order, page)}>address</th>
+            <th onClick={() => sortBy('email', order, page)}>email</th>
+            <th onClick={() => sortBy('company', order, page)}>company</th>
+            <th onClick={() => sortBy('phone', order, page)}>phone</th>
+            <th onClick={() => sortBy('website', order, page)}>website</th>
+          </tr>
+          {data && data.map((item: IUser) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.username}</td>
+                <td>{item.address.street}</td>
+                <td>{item.email}</td>
+                <td>{item.company.name}</td>
+                <td>{item.phone}</td>
+                <td>{item.website}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <div className="button-wrapper">
+        <button onClick={() => setPage((prev) => {
+          return prev - 1 < 1 ? 1 : prev - 1
+        })}>Prev</button>
+        <span>{page}</span>
+        <button onClick={() => setPage(prev => prev + 1)}>Next</button>
+      </div>
     </div>
   );
 }
---
+++ src/db.tsx
@@ -0,0 +1,27 @@
+
+
+export class db {
+
+  private static _baseUrl: string = 'http://localhost:3001/'
+
+  static async getUsers(page: number) {
+    let res = await fetch(`${this._baseUrl}users?_limit=5&_page=${page}`)
+    if (!res.ok) {
+      throw new Error(`could not fetch ${this._baseUrl}users, received ${res.status}`);
+    }
+    return await res.json();
+  }
+
+  static async sortBy(param: string, order: string, page: number) {
+    let res = await fetch(`${this._baseUrl}users?_sort=${param}&_order=${order}&_limit=5&_page=${page}`)
+    return await res.json();
+  }
+
+  static async search(query: string) {
+    let res = await fetch(`${this._baseUrl}users?q=${query}&_limit=5`)
+    if (!res.ok) {
+      throw new Error(`could not fetch ${this._baseUrl}users, received ${res.status}`);
+    }
+    return await res.json();
+  }
+}
